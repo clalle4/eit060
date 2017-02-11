62be489cae1359eeb86b48dd233bed6d86ba7786
sven <sven_andersen.sa@hotmail.com>
2017-02-11 17:37:59 +0100

    hade inte sparat main

--- src/server/Main.java
+++ src/server/Main.java
@@ -4,16 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("godkänt, you can all go home now   ヽ(͡◕ ͜ʖ ͡◕)ﾉ");
-	//	Hub h = new Hub();
-		 test(); // use to test your methods, if u work on "Main" just remove
+		Hub h = new Hub();
+	//	 test(); // use to test your methods, if u work on "Main" just remove
 		// this
 	}
 
-	 public static void test(){
-		 Doctor a = new Doctor("a");
-		 if(a.createPatient("ABBA")){
-			 System.out.println("success");
-		 }else{System.out.println("denied");}
-	 }
+//	 public static void test(){
+//		
+//	 }
 
 }
