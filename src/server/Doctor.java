634e829415bb44e6e5aa9152d10443eca4004cb2
sven <sven_andersen.sa@hotmail.com>
2017-02-11 17:33:30 +0100

    doctor create method

--- src/server/Doctor.java
+++ src/server/Doctor.java
@@ -1,18 +1,43 @@
 package server;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Doctor extends Nurse {
 
 	public Doctor(String name) {
 		super(name);
 		// TODO Auto-generated constructor stub
 	}
-	
-	/** In addition, the doctor can create new records for a patient provided
+
+	/**
+	 * In addition, the doctor can create new records for a patient provided
 	 * that the doctor is treating the patient. When creating the record, the
 	 * doctor also associates a nurse with the record.
 	 */
-	public void createPatient() {
-		
+	public boolean createPatient(String FILENAME) {
+		File usersDir = new File("./files/PatientRecords/");
+		File[] userlist = usersDir.listFiles();
+
+		if (userlist != null) {
+			for (File fil : userlist) {
+				if (FILENAME.equals(fil.getName().replaceAll(".txt", ""))) {
+					return false;
+				}
+			}
+		}
+		try {
+			StringBuilder sb = new StringBuilder("./files/PatientRecords/");
+			sb.append(FILENAME);
+			sb.append(".txt");
+			PrintWriter writer = new PrintWriter(sb.toString(), "UTF-8");
+			writer.close();
+			return true;
+		} catch (IOException e) {
+			// do something
+		}
+		return false;
 	}
 
 }
--- src/server/Main.java
+++ src/server/Main.java
@@ -4,12 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("godkänt, you can all go home now   ヽ(͡◕ ͜ʖ ͡◕)ﾉ");
-		Hub h = new Hub();
-		// test(); // use to test your methods, if u work on "Main" just remove
+	//	Hub h = new Hub();
+		 test(); // use to test your methods, if u work on "Main" just remove
 		// this
 	}
 
-	// public static void test(){
-	// }
+	 public static void test(){
+		 Doctor a = new Doctor("a");
+		 if(a.createPatient("ABBA")){
+			 System.out.println("success");
+		 }else{System.out.println("denied");}
+	 }
 
 }
