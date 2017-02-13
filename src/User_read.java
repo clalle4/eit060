741f925aeba1f54df03233f3f84cd87490780095
taalanim <jap13san@student.lu.se>
2017-02-13 16:01:40 +0100

    gov read test
    coverage increase

--- src/User_read.java
+++ src/User_read.java
@@ -11,30 +11,38 @@ public class User_read {
 	@Test
 	public void test_UserReadPatient1() {
 		Patient a = new Patient("Alice");
-			assertEquals(a.read("Alice"), "Alice Info"); 
-		
+		assertEquals(a.read("Alice"), "Alice Info");
+
 	}
+
 	@Test
 	public void test_UserReadPatient2() {
 		Patient a = new Patient("Alice");
-			assertEquals(a.read("Bob"), "You are not allowed to access that file"); 
-		
+		assertEquals(a.read("Bob"), "You are not allowed to access that file");
+
 	}
+
 	@Test
 	public void test_UserReadNurse1() {
 		ArrayList<Division> b = new ArrayList<Division>();
 		b.add(new Division("D1"));
 		Nurse a = new Nurse("Nurse1", b);
-			assertEquals(a.read("Connor"), "You are not allowed to access that file");
-		
+		assertEquals(a.read("Connor"), "You are not allowed to access that file");
+
 	}
+
 	@Test
 	public void test_UserReadNurse2() {
 		ArrayList<Division> b = new ArrayList<Division>();
 		b.add(new Division("D1"));
 		Nurse a = new Nurse("Nurse1", b);
-			assertEquals(a.read("Bob"), "Bob Info");
-		
+		assertEquals(a.read("Bob"), "Bob Info");
+
 	}
 
+	@Test
+	public void test_UserReadGov1() {
+		Gov a = new Gov("adam");
+		assertEquals(a.read("Alice"), "Alice Info");
+	}
 }
